8faa5f885dba9e92d12db199b510bbd1cd4cd59d
Nicolás Serna <nicolito128@hotmail.com>
2020-05-12 00:05:15 -0300

    Add func Byte

--- each/each.go
+++ each/each.go
@@ -9,6 +9,9 @@ type HandlerFloat func(element float64, index int, array []float64) float64
 // HandlerString ... A callback for []string
 type HandlerString func(element string, index int, array []string) string
 
+// HandlerByte ... A callback for []byte
+type HandlerByte func(element byte, index int, array []byte) byte
+
 // Int ... Iterate a []int
 func Int(arr []int, handler HandlerInt) []int {
 	var newArr []int
@@ -39,6 +42,19 @@ func Float(arr []float64, handler HandlerFloat) []float64 {
 func String(arr []string, handler HandlerString) []string {
 	var newArr []string
 	var element string
+
+	for index := range arr {
+		element = arr[index]
+		newArr = append(newArr, handler(element, index, arr))
+	}
+
+	return newArr
+}
+
+// Byte ... Iterate a []byte
+func Byte(arr []byte, handler HandlerByte) []byte {
+	var newArr []byte
+	var element byte
 
 	for index := range arr {
 		element = arr[index]
