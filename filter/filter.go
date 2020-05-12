bc7954124b66b1b6863a6c37ed432043a9949260
Nicolás Serna <nicolito128@hotmail.com>
2020-05-12 02:08:47 -0300

    Add filter

---
+++ filter/filter.go
@@ -0,0 +1,20 @@
+package filter
+
+// CallbackInt ... Iterate a []int
+type CallbackInt func(element int, index int, arr []int) bool
+
+// Int ... Evaluates a boolean to enter an element in a new []int
+func Int(arr []int, callback CallbackInt) []int {
+	var filteredArr []int
+	var element int
+	var condition bool
+
+	for index := range arr {
+		element = arr[index]
+		condition = callback(element, index, arr)
+
+		if condition {
+			filteredArr = append(filteredArr, element)
+		}
+	}
+}
