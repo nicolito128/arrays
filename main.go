9191fec2e664b1b8f651ab9bf58ca6db30f23a1e
Nicolás Serna <nicolito128@hotmail.com>
2020-05-12 02:24:13 -0300

    Update main.go

--- main.go
+++ main.go
@@ -5,10 +5,11 @@ import (
 	"strings"
 
 	"github.com/nicolito128/arrays/each"
+	"github.com/nicolito128/arrays/filter"
 )
 
 func main() {
-	nums := []int{3, 4, 1, 1, 9, 8, 7, 7, 6, 5, 5}
+	nums := []int{3, 4, 1, 9, 9, 8, 7, 7, 6, 5, 5}
 	nums2 := each.Int(nums, mult)
 	fmt.Println(nums)
 	fmt.Println(nums2)
@@ -17,6 +18,13 @@ func main() {
 
 	str := []string{"wallace", "dog", "carl", "earth"}
 	fmt.Println(each.String(str, upper))
+
+	fmt.Printf("\n/*******/\n\n")
+
+	evenNums := filter.Int(nums, filterEvenNumbers)
+	divisibleByThree := filter.Int(nums, divByThree)
+	fmt.Println(evenNums)
+	fmt.Println(divisibleByThree)
 }
 
 func mult(el int, i int, arr []int) int {
@@ -29,4 +37,12 @@ func mult(el int, i int, arr []int) int {
 
 func upper(el string, i int, arr []string) string {
 	return strings.ToUpper(el)
+}
+
+func filterEvenNumbers(el int, i int, arr []int) bool {
+	return (el % 2) == 0
+}
+
+func divByThree(el int, i int, arr []int) bool {
+	return (el % 3) == 0
 }
